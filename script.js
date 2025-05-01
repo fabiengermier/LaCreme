3b57d2bae1411d02ce47ec011dbb2c7f03173de2
Darkvaduck_ <fabien.germier@hotmail.fr>
2025-05-02 01:08:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ const clips = await Promise.all(staticUsers.map(async (login) => {
       thumbnail_url: clip.thumbnail_url,
       url: clip.url
     } : null;
-  }));
+  };
 
   document.getElementById("streamers").innerHTML = streamers.map(stream => `
     <a href="https://twitch.tv/${stream.user_name}" target="_blank" class="border bg-white p-4 rounded shadow hover:shadow-lg">
